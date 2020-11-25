91991fd96f9e219f7e6a5ca27c95da3ef1c91fe2
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2020-11-25 15:58:23 +0900

    image-fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
-  has_one :purchase
-  belongs_to :brand
-  belongs_to :category
-  has_one_active_hash :cost
-  has_one_active_hash :days_to_ship
-  belongs_to :seller, class_name: :user
-  belongs_to :buyer, class_name: :user
+  # has_one :purchase
+  # belongs_to :brand
+  # belongs_to :category
+  # has_one_active_hash :cost
+  # has_one_active_hash :days_to_ship
+  # belongs_to :seller, class_name: :user
+  # belongs_to :buyer, class_name: :user
 end
